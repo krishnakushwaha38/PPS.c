7e0aec41547f66bc1c7350466bb311c60e4593f7
krishnakushwaha38 <krishnakushwaha23813@gmail.com>
2026-02-21 21:07:25 +0530

    practical1.c

---
+++ practical1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  printf("\narethmatic oprations");
+  int a=8;
+  int b=3;
+  printf("\naddion is =%d", a + b );
+  printf("\nsubtraction is =%d", a - b );
+  printf("\nmultiplication is =%d", a * b );
+  printf("\ndivison is =%d", a / b );
+  printf("\nreminder is =%d", a % b );
+
+  return 0;
+}
